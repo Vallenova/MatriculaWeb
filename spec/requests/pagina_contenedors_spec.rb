de0684d1a6e733725d314bd161d8c83a1b7a33f4
Dante Alferez Condori <dante.alferez@vallenova.pe>
2014-06-11 19:41:53 -0500

    Prueba de titulo crear apoderado

--- spec/requests/pagina_contenedors_spec.rb
+++ spec/requests/pagina_contenedors_spec.rb
@@ -1,8 +1,16 @@
 require 'spec_helper'
 
 describe "Página Contenedor" do
-  it "deberia tener el contenido 'Tacna-Perú'" do
-    visit '/pagina_contenedor/home'
-    expect(page).to have_content('Tacna-Perú')
+  describe "Crear Apoderado" do
+    it "deberia tener el contenido 'Tacna-Perú'" do
+      visit '/pagina_contenedor/home'
+      expect(page).to have_content('Tacna-Perú')
+      expect(page).to have_content('Asistente')
+    end
+
+    it "deberia tener el título 'Crear Apoderado'" do
+      visit '/pagina_contenedor/home'
+      expect(page).to have_title("Crear Apoderado | Sistema Matrícula")
+    end
   end
 end
